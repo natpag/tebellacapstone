262bf246cd9ede38fc9627fb02174577dd3dfd5b
npagan <npagan@proforma.com>
2020-04-13 11:15:59 -0400

    addedCssandEmptyImageBox

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Logo from '../components/images/logo.png'
 import '../custom.scss'
 
 export function Home() {
   return (
-    <body>
+    <section className="body">
       <section className="randomizePage">
         <header className="header">
           <NavBar />
         </header>
+        <main>
+          <section className="hero">
+            <img className="logo" src={Logo} alt="Logo" />
+          </section>
+          <button className="randomizer">Random Tea</button>
+          <section className="teaBox">
+            <section className="teaHeader">
+              <section className="teaFamilyLabel"> {/* TeaFamily */} B</section>
+              <section className="teaFamilyColor">
+                {/* TeaFamilyColor */}.
+              </section>
+              <section className="teaNameLabel">
+                {/* TeaName */} Earl Grey Lavender
+              </section>
+            </section>
+            <section className="teaImage">{/* TeaPicture */} img</section>
+          </section>
+        </main>
       </section>
-    </body>
+    </section>
   )
 }
