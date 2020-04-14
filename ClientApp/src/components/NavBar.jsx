3a1d90b34aa4592df1550beca22a3fa810d97ddd
npagan <npagan@proforma.com>
2020-04-13 23:15:19 -0400

    addedNavIcons

--- ClientApp/src/components/NavBar.jsx
+++ ClientApp/src/components/NavBar.jsx
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import Logo from './images/tebellaLogoPic.jpg'
 import '../custom.scss'
 
 const NavBar = () => {
   return (
     <nav className="navBar">
-      <ul>
-        <li>Home</li>
-        <li>Account</li>
-        <li>Tea History</li>
-        <li>Tea Log</li>
-      </ul>
+      <section>
+        <ul>
+          <li>
+            <i className="fas fa-home"></i>
+          </li>
+          <li>
+            <i className="fas fa-mug-hot"></i>
+          </li>
+          <li>
+            <i className="fas fa-clipboard-list"></i>
+          </li>
+        </ul>
+      </section>
+
+      <section className="rightNav">
+        <ul>
+          <li>
+            <i className="far fa-user-circle"></i>
+          </li>
+        </ul>
+      </section>
     </nav>
   )
 }
--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Logo from '../components/images/logo.png'
 import blackEarlGreyLavender from '../components/images/blackEarlGreyLavender.jpg'
+import FloridaOrangeBlossom from '../components/images/FloridaOrangeBlossom.jpg'
 import '../custom.scss'
 
 export function Home() {
@@ -15,28 +16,12 @@ export function Home() {
           <section className="hero">
             <img className="logo" src={Logo} alt="Logo" />
           </section>
+
           <button className="randomizer">Random Tea</button>
-          <section className="teaBox">
-            <section className="teaHeader">
-              <section className="teaFamilyLabel"> {/* TeaFamily */} B</section>
-              <section className="teaFamilyColor">
-                {/* TeaFamilyColor */}.
-              </section>
-              <section className="teaNameLabel">
-                {/* TeaName */} Florida Orange Blossom
-              </section>
-            </section>
-            <section className="teaImage">
-              <img
-                /* TeaPicture */ className="teaImg"
-                src={blackEarlGreyLavender}
-                alt="image"
-              />
-            </section>
-          </section>
+
           <section className="specialTeaRandomizer">
             <button className="specialRandomizer">Special Random Tea</button>
-            <section className="selectorSection">
+            <section className="selectorLabelSection">
               <section className="selectorLabel">Tea Family</section>
               <section className="selectorLabel">Type</section>
               <section className="selectorLabel">Flavor</section>
@@ -53,10 +38,12 @@ export function Home() {
 
               <select>
                 <option value="0"></option>
-                <option value="1">White</option>
-                <option value="2">Black</option>
-                <option value="3">Green</option>
-                <option value="4">Oolong</option>
+                <option value="1">Flavored</option>
+                <option value="2">Organic</option>
+                <option value="3">Reserved</option>
+                <option value="4">Seasonal</option>
+                <option value="4">Scented</option>
+                <option value="4">Traditional</option>
               </select>
 
               <select>
@@ -66,6 +53,24 @@ export function Home() {
                 <option value="3">Green</option>
                 <option value="4">Oolong</option>
               </select>
+            </section>
+          </section>
+          <section className="teaBox">
+            <section className="teaHeader">
+              <section className="teaFamilyLabel"> {/* TeaFamily */} B</section>
+              <section className="teaFamilyColor">
+                {/* TeaFamilyColor */}.
+              </section>
+              <section className="teaNameLabel">
+                {/* TeaName */} Earl Grey Lavender
+              </section>
+            </section>
+            <section className="teaImage">
+              <img
+                /* TeaPicture */ className="teaImg"
+                src={blackEarlGreyLavender}
+                alt="image"
+              />
             </section>
           </section>
         </main>
