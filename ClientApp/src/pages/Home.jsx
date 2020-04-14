b14661e9a5d6f2e9156ffe0c5201ec412e2b6bb8
npagan <npagan@proforma.com>
2020-04-13 21:04:15 -0400

    finishedSpecialTeaRadnomizerCss

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Logo from '../components/images/logo.png'
+import blackEarlGreyLavender from '../components/images/blackEarlGreyLavender.jpg'
 import '../custom.scss'
 
 export function Home() {
@@ -22,10 +23,50 @@ export function Home() {
                 {/* TeaFamilyColor */}.
               </section>
               <section className="teaNameLabel">
-                {/* TeaName */} Earl Grey Lavender
+                {/* TeaName */} Florida Orange Blossom
               </section>
             </section>
-            <section className="teaImage">{/* TeaPicture */} img</section>
+            <section className="teaImage">
+              <img
+                /* TeaPicture */ className="teaImg"
+                src={blackEarlGreyLavender}
+                alt="image"
+              />
+            </section>
+          </section>
+          <section className="specialTeaRandomizer">
+            <button className="specialRandomizer">Special Random Tea</button>
+            <section className="selectorSection">
+              <section className="selectorLabel">Tea Family</section>
+              <section className="selectorLabel">Type</section>
+              <section className="selectorLabel">Flavor</section>
+            </section>
+
+            <section className="selectorSection">
+              <select>
+                <option value="0"></option>
+                <option value="1">White</option>
+                <option value="2">Black</option>
+                <option value="3">Green</option>
+                <option value="4">Oolong</option>
+              </select>
+
+              <select>
+                <option value="0"></option>
+                <option value="1">White</option>
+                <option value="2">Black</option>
+                <option value="3">Green</option>
+                <option value="4">Oolong</option>
+              </select>
+
+              <select>
+                <option value="0"></option>
+                <option value="1">White</option>
+                <option value="2">Black</option>
+                <option value="3">Green</option>
+                <option value="4">Oolong</option>
+              </select>
+            </section>
           </section>
         </main>
       </section>
