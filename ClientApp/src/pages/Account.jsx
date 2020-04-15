53174ada3777334c56a538a97dcb679bae6e8bfc
npagan <npagan@proforma.com>
2020-04-15 09:45:15 -0400

    addedAccountMobile

--- ClientApp/src/pages/Account.jsx
+++ ClientApp/src/pages/Account.jsx
@@ -10,9 +10,57 @@ export function Account() {
         <header className="header">
           <NavBar />
         </header>
-        <main>
-          <section className="pageTitle">Account</section>
-        </main>
+
+        <section className="pageTitle">
+          <section>Account</section>
+        </section>
+        <section className="accountBody">
+          <section className="nameSection">
+            <section className="inputField">
+              <ul>
+                <li>First Name</li>
+                <li>
+                  <input className="nameInput"></input>
+                </li>
+              </ul>
+            </section>
+            <section className="inputField">
+              <ul>
+                <li>Last Name</li>
+                <li>
+                  <input className="nameInput"></input>
+                </li>
+              </ul>
+            </section>
+          </section>
+          <section className="accountSection">
+            <section className="inputField">
+              <ul>
+                <li>Phone Number</li>
+                <li>
+                  <input className="longInput"></input>
+                </li>
+              </ul>
+            </section>
+            <section className="inputField">
+              <ul>
+                <li>Email</li>
+                <li>
+                  <input className="longInput"></input>
+                </li>
+              </ul>
+            </section>
+            <section className="inputField">
+              <ul>
+                <li>Password</li>
+                <li>
+                  <input className="longInput"></input>
+                </li>
+              </ul>
+            </section>
+          </section>
+          <button className="saveChanges">Save Changes</button>
+        </section>
       </section>
     </section>
   )
