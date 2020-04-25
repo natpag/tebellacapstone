7da3d865150d5112b6612713fd17147ba60d9692
npagan <npagan@proforma.com>
2020-04-24 20:28:06 -0400

    editedUserTable

---
+++ Migrations/20200425002421_EditedUserTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeBellaCapstone.Migrations
+{
+    public partial class EditedUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HashedPassword",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HashedPassword",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,7 +7,7 @@ namespace TeBellaCapstone.Models
     public string LastName { get; set; }
     public string PhoneNumber { get; set; }
     public string Email { get; set; }
-    public string Password { get; set; }
+    public string HashedPassword { get; set; }
 
   }
 }
