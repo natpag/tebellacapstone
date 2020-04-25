1499446351acd46935520d428386f7640eb37ba6
npagan <npagan@proforma.com>
2020-04-24 22:53:37 -0400

    addedStateToSignUp

--- ClientApp/src/pages/SignUp.jsx
+++ ClientApp/src/pages/SignUp.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import { Link } from 'react-router-dom'
 import '../custom.scss'
+import Axios from 'axios'
 
 export function SignUp() {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const sendNewUserToApi = async () => {
+    const resp = await Axios.post('/auth/signup', {
+      firstName: firstName,
+      lastName: lastName,
+      phoneNumber: phoneNumber,
+      email: email,
+      password: password,
+    })
+    console.log(resp.data)
+  }
+
   return (
     <section className="body">
       <section className="randomizePage">
@@ -21,7 +39,12 @@ export function SignUp() {
               <ul>
                 <li>First Name</li>
                 <li>
-                  <input className="nameInput"></input>
+                  <input
+                    className="nameInput"
+                    type="text"
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)}
+                  />
                 </li>
               </ul>
             </section>
@@ -29,7 +52,12 @@ export function SignUp() {
               <ul>
                 <li>Last Name</li>
                 <li>
-                  <input className="nameInput"></input>
+                  <input
+                    className="nameInput"
+                    type="text"
+                    value={lastName}
+                    onChange={e => setLastName(e.target.value)}
+                  />
                 </li>
               </ul>
             </section>
@@ -37,7 +65,12 @@ export function SignUp() {
               <ul>
                 <li>Phone Number</li>
                 <li>
-                  <input className="nameInput"></input>
+                  <input
+                    className="nameInput"
+                    type="text"
+                    value={phoneNumber}
+                    onChange={e => setPhoneNumber(e.target.value)}
+                  />
                 </li>
               </ul>
             </section>
@@ -45,7 +78,12 @@ export function SignUp() {
               <ul>
                 <li>Email</li>
                 <li>
-                  <input className="nameInput"></input>
+                  <input
+                    className="nameInput"
+                    type="text"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
                 </li>
               </ul>
             </section>
@@ -53,12 +91,18 @@ export function SignUp() {
               <ul>
                 <li>Password</li>
                 <li>
-                  <input className="nameInput"></input>
+                  <input
+                    className="nameInput"
+                    type="password"
+                    type="text"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                  />
                 </li>
               </ul>
             </section>
           </section>
-          <button className="saveChanges">
+          <button className="saveChanges" onClick={sendNewUserToApi}>
             <Link to="/account">Save Changes</Link>
           </button>
         </section>
