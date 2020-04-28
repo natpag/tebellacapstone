c65fe9d6200511d20c4899fa436d98027924328c
npagan <npagan@proforma.com>
2020-04-27 21:00:41 -0400

    addedFavoriteTeaToReviewController

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -91,28 +91,28 @@ namespace TeBellaCapstone.Controllers
 
       return Ok(review);
     }
+
+
+    [HttpPost("FavoriteTea")]
+    public async Task<ActionResult<FavoriteTeasModel>> FavoriteTeaForUser(FavoriteTeasModel favoriteTeasModel)
+    {
+      // get the user Id form the User Object
+      var userId = _userService.GetCurrentUserId(User);
+
+      // create a new bookmark 
+      var favorite = new FavoriteTea
+      {
+        TeaId = favoriteTeasModel.TeaId,
+        UserId = userId
+      };
+      //save it our database
+      _context.FavoriteTeas.Add(favorite);
+      await _context.SaveChangesAsync();
+      // return something?
+      return Ok(favorite);
+    }
+
   }
-
-  //   [HttpPost("{reviewId}")]
-  //    public async Task<ActionResult<FavoriteTeasModel>> FavoriteTeaForUser(FavoriteTeasModel favoriteTeasModel)
-  //   {
-  //     // get the user Id form the User Object
-  //     var userId = _userService.GetCurrentUserId(User);
-
-  //     // create a new bookmark 
-  //     var favorite = new FavoriteTeasModel
-  //     {
-  //       TeaId = favoriteTeasModel.TeaId,
-  //       UserId = userId
-  //     };
-  //     //save it our database
-  //     _context.FavoriteTeas.Add(favorite);
-  //     await _context.SaveChangesAsync();
-  //     // return something?
-  //     return Ok(favorite);
-  //   }
-
-  // }
 
 
   // // DELETE: api/Trails/5
