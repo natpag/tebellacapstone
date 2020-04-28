851470f20501d6a51f99434e9a464634d60b64e4
npagan <npagan@proforma.com>
2020-04-27 20:43:25 -0400

    addedFavTeasModeledit

--- ClientApp/ViewModels/FavoriteTeasModel.cs
+++ ClientApp/ViewModels/FavoriteTeasModel.cs
@@ -5,7 +5,7 @@ namespace TeBellaCapstone.Models
   public class FavoriteTeasModel
   {
     public int TeaId { get; set; }
-    public string UserId { get; set; }
+    public int UserId { get; set; }
 
   }
 }
